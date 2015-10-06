daef578d0e8f45c9f7c5db0ea1afa60883649e0c
Milo <milonice@hotmail.com>
2015-10-06 12:10:38 +0200

    Add: Getting the letter-tiles in place and minor tile-styling.

--- js/main.js
+++ js/main.js
@@ -2,4 +2,42 @@ $( function() {
 
   console.log( "jQuery loaded." )
 
+  var $letterzone = $( '.letterzone' ),
+      $startTag = '<div class="letter">',
+      $endTag = '</div>'
+
+  var alphabet = [
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+    "G",
+    "H",
+    "I",
+    "J",
+    "K",
+    "L",
+    "M",
+    "N",
+    "O",
+    "Q",
+    "R",
+    "S",
+    "T",
+    "U",
+    "V",
+    "X",
+    "Y",
+    "Z",
+    "Å",
+    "Ä",
+    "Ö"
+  ];
+
+  for( var i = 0; i < alphabet.length; i++ ) {
+    $letterzone.append( $startTag + alphabet[i] + $endTag )
+  }
+
 })
