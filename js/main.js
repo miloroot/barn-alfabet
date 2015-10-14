cf682d561888d5e335235bfbdd0b953c3a7ccb46
Milo <milonice@hotmail.com>
2015-10-14 15:07:58 +0200

    Fix: Added semicolons at the end of lines. Better off that way.

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
 $( function() {
 
-  console.log( "jQuery loaded." )
+  console.log( "jQuery loaded." );
 
   var $letterzone = $( '.letterzone' ),
       $startTag = '<div class="letter">',
       $endTag = '</div>',
       $letter = $( '.letter' ),
-      $writer = $( '.writer')
+      $writer = $( '.writer');
 
   var alphabet = [
     "A",
@@ -42,20 +42,20 @@ $( function() {
 
   for( var i = 0; i < alphabet.length; i++ ) {
     $letterzone.append( $startTag + alphabet[i] + $endTag )
-  }
+  };
 
-  $letterzone.on( 'click', getLetter )
+  $letterzone.on( 'click', getLetter );
 
   function getLetter( e ) {
-    console.log( 'the letter clicked: ', e.target.innerHTML )
-    console.log( 'the length of innerHTML: ', e.target.innerHTML.length )
+    console.log( 'the letter clicked: ', e.target.innerHTML );
+    console.log( 'the length of innerHTML: ', e.target.innerHTML.length );
 
     if( e.target.innerHTML.length < 2 ) {
-      $writer.append( e.target.innerHTML )
+      $writer.append( e.target.innerHTML );
     }
     else {
-      $writer.append( " " )
+      $writer.append( " " );
     }
   }
 
-})
+});
